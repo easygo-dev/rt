61179877cc28cf1e1677b181bf1b829f35504bc7
easygo-dev <85024078+easygo-dev@users.noreply.github.com>
2024-12-26 17:55:18 +0300

    Update snapshot_sync.sh

--- snapshot_sync.sh
+++ snapshot_sync.sh
@@ -12,11 +12,15 @@ edit_config_file() {
   # Удаление старого блока "snapshot_sync"
   sed -i '/"snapshot_sync": {/,/}/d' "$file"
 
-  # Корректная вставка блока "snapshot_sync" перед закрывающей фигурной скобкой объекта "chain"
+  # Корректная вставка блока "snapshot_sync" после закрытия блока "wallet" и перед закрытием блока "chain"
   awk '
     BEGIN { inside_chain = 0 }
     /"chain": {/ { inside_chain = 1 }
-    inside_chain && /^\s*}/ { inside_chain = 0; print "        \"snapshot_sync\": {\n          \"sleep\": 1.5,\n          \"batch_size\": 10000,\n          \"starting_sub_id\": 0,\n          \"sync_period\": 1\n        }," }
+    inside_chain && /"wallet": {/,/}/ { print; next } # Пропускаем блок "wallet"
+    inside_chain && /^\s*}/ && !printed { 
+      printed = 1; 
+      print "        \"snapshot_sync\": {\n          \"sleep\": 1.5,\n          \"batch_size\": 10000,\n          \"starting_sub_id\": 0,\n          \"sync_period\": 1\n        }," 
+    }
     { print }
   ' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
 
